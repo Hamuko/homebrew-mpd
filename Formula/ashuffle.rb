7db7e3a987c75fb8cfd0f47fe79acb0c058ce415
Hamuko <1270914+Hamuko@users.noreply.github.com>
2023-05-09 04:33:12 +0000

    ashuffle: update 3.13.6 bottle.

--- Formula/ashuffle.rb
+++ Formula/ashuffle.rb
@@ -9,9 +9,9 @@ class Ashuffle < Formula
   head "https://github.com/joshkunz/ashuffle.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/Hamuko/homebrew-mpd/releases/download/ashuffle-3.13.5"
-    sha256 cellar: :any,                 big_sur:      "a7efdcd655601fc1b5695c5b8e52674f7c33973662edbb934db187aaf4b351aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7853831465745579f4a5380432c887a7ee23d2ee731fc0ae79d7b31bb3d929b1"
+    root_url "https://github.com/Hamuko/homebrew-mpd/releases/download/ashuffle-3.13.6"
+    sha256 cellar: :any,                 monterey:     "28d386a0df61dbee8ca289c54e17b1215114fa77ba862a764fcb9631013e6bbe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8c81902c19d63257d63eb0b6659b7003fc1143fbfb2032b3ff9e909fafcb83cb"
   end
 
   depends_on "cmake" => :build
